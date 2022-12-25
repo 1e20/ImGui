037b94d5c2436e3fbadabe0875e048cca36d1b66
1e20 <98073814+1e20@users.noreply.github.com>
2022-12-25 04:31:29 -0800

    Create Drawing.lua

---
+++ Modules/Drawing.lua
@@ -0,0 +1,121 @@
+-- Better Drawing Library by Require <3
+
+--[[
+    How to use: 
+
+    local Brush = Brush.New{};
+
+    Brush:Draw(Object, 'Line', LineProperties);
+    Brush:EndDraw(); -- De Queues Drawings (must do after drawing)
+]]
+
+local CORE = game:GetService("CoreGui");
+local CAMERA = workspace.CurrentCamera; 
+local DRAW = Drawing.new;
+
+local Brush = { };
+Brush.Memory = { Pool = { } };
+
+function Brush.New()
+    local NewBrush = {};
+
+    for c, k in next, Brush do 
+        NewBrush[c] = k; 
+    end; 
+
+    return NewBrush;
+end;
+
+function Brush.Memory:ValidateBuffer(Buffer)
+    if (not Buffer.Object or not Buffer.Object.Parent or not Buffer.Object:IsDescendantOf(workspace)) then 
+        return false;
+    end;
+
+    return true; 
+end; 
+
+function Brush.Memory:CreateBuffer(Object)
+    if (not Object) then return false end;
+
+    local Buffer = {
+        Id = #self.Pool + 1;
+        Object = Object;
+        Queue = { 
+            Line = { }; 
+            Square = { };
+            Circle = { };
+            Quad = { };
+            Text = { };
+            Triangle = { };
+        };
+        Drawings = { 
+            Line = { }; 
+            Square = { };
+            Circle = { };
+            Quad = { };
+            Text = { };
+            Triangle = { };
+        };
+    };
+
+    self.Pool[Object] = Buffer; 
+    return Buffer;
+end; 
+
+function Brush.Memory:DeQueueBuffer(Buffer)
+    for i, c in next, Buffer.Queue do 
+        for y, b in next, c do 
+            table.remove(c, y); 
+            table.insert(Buffer.Drawings[i], b);
+        end; 
+    end; 
+end; 
+
+function Brush.Memory:Allocate(Object, Class, Properties)
+    if (not Object) then return false end;
+    local Buffer = self.Pool[Object]; 
+
+    if (not Buffer) then 
+        Buffer = self:CreateBuffer(Object); 
+    end; 
+
+    local Drawing = Buffer.Drawings[Class][1];
+
+    if (not Drawing) then 
+        Drawing = DRAW(Class); 
+        table.insert(Buffer.Queue[Class], Drawing);
+    else 
+        table.remove(Buffer.Drawings[Class], 1);
+        table.insert(Buffer.Queue[Class], Drawing); 
+    end; 
+
+    for p, v in next, Properties do 
+        Drawing[p] = v;
+    end; 
+
+end; 
+
+function Brush:EndDraw()
+    for i, m in next, self.Memory.Pool do 
+        self.Memory:DeQueueBuffer(m);
+
+        if (not self.Memory:ValidateBuffer(m)) then 
+            for _, c in next, m.Drawings do 
+                for _, drawing in next, c do drawing:Remove() end; 
+            end; 
+    
+            for _, c in next, m.Queue do 
+                for _, drawing in next, c do drawing:Remove() end; 
+            end; 
+    
+            self.Memory.Pool[m.Object] = nil; 
+        end;
+    end; 
+end; 
+
+function Brush:Draw(Object, Class, Properties)
+    assert((Class and Properties), "[Brush-Draw] Missing arguments");
+    self.Memory:Allocate(Object, Class, Properties);
+end; 
+
+return Brush;
